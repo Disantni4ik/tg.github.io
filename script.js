8f80cff3604ad6293669d3777c2f4394ce8c7178
Disantni4ik <dimasic16252@gmail.com>
2025-08-05 03:41:38 +0300

    big changes

--- script.js
+++ script.js
@@ -1,21 +1,14 @@
-window.addEventListener("DOMContentLoaded", () => {
-  const tg = window.Telegram.WebApp;
+const continueBtn = document.getElementById("continue1");
+const start = document.getElementById("start");
+const registration = document.getElementById("registration");
 
-  if (tg) {
-    tg.ready(); // повідомляємо, що WebApp готовий
-    tg.expand(); // просимо Telegram показати повну висоту
+continueBtn.addEventListener("click", () => {
+    start.classList.add("hidden");
+    start.classList.remove("visible");
 
-    // Використовуємо значення тільки після того, як WebApp точно готовий
-    const width = tg.viewportWidth;
-    document.getElementById("width").textContent = width + "px";
-
-    // Якщо потрібно, увімкни обробку кнопки
-    // document.getElementById("continueBtn").addEventListener("click", () => {
-    //   console.log("continueBtn clicked");
-    //   document.getElementById("form").style.display = "block";
-    //   document.getElementById("start").style.display = "none";
-    // });
-  } else {
-    console.warn("WebApp не запущено в Telegram.");
-  }
-});
+    setTimeout(() => {
+      start.style.display = "none";
+      registration.classList.remove("hidden");
+      registration.classList.add("visible");
+    }, 300);
+  });

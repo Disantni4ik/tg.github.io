ced8c520892d4d4ad7942ce39b13eebf96322241
Disantni4ik <dimasic16252@gmail.com>
2025-08-05 04:31:45 +0300

    big big changes

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const start = document.getElementById("start");
 const registration = document.getElementById("registration");
 
 continueBtn.addEventListener("click", () => {
+    registration.style.display = "block";
     start.classList.add("hidden");
     start.classList.remove("visible");
 
@@ -12,3 +13,20 @@ continueBtn.addEventListener("click", () => {
       registration.classList.add("visible");
     }, 300);
   });
+
+const copyButton = document.querySelector('.copy-button');
+const message = document.getElementById('copyMessage');
+
+copyButton.addEventListener('click', () => {
+      const textToCopy = copyButton.textContent.trim();
+
+      navigator.clipboard.writeText(textToCopy).then(() => {
+        message.classList.add('show');
+
+        setTimeout(() => {
+          message.classList.remove('show');
+        }, 2000);
+      }).catch(err => {
+        console.error('Помилка копіювання: ', err);
+      });
+    });
